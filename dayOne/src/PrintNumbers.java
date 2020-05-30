1178060c76ee8bb113d560cba745d3f42570f814
Mohamed MEKOUAR <thelodge.oasis@gmail.com>
2020-05-30 09:04:54 +0000

    Write code to print the following pattern

--- dayOne/src/PrintNumbers.java
+++ dayOne/src/PrintNumbers.java
@@ -2,7 +2,7 @@ public class PrintNumbers {
 
     public static void main(String[] args){
     PrintNumbers P=new PrintNumbers();
-    P.printAll(2);
+    P.printAll(5);
 
     }
 
@@ -10,15 +10,20 @@ public class PrintNumbers {
         if (n<=0) throw new IllegalArgumentException("Parameter N should be higher than 0");
         else for (int i=1;i<=n;i++)
         {
-            for (int j=1;j<n-i;j++) System.out.print(' '); printLine(i);System.out.println();
+            for (int j=1;j<=n-i;j++) System.out.print(' '); printLine(i);System.out.println();
 
         }
 
     }
 
     private void printLine(int n){
-        for (int i=0;i<n;i++) System.out.print(n+i);
-        for (int i=1;i<n;i++) System.out.print(n-i);
+
+        int count=0;
+        for ( count=0;count<n;count++) {
+            System.out.print(n+count);
+        }
+        count=n+count;
+        for (int i=1;i<n;i++) System.out.print(count-i-1);
 
 
     }
